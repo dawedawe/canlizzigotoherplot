17008e4a54ff65fc4108ae3e6c25088cadc617ca
dawe <dawedawe@posteo.de>
2024-07-24 00:59:55 +0200

    flowers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ const App: FunctionComponent = (): ReactElement => {
 
     return (
         <main>
-            <h1>Can Lizzi go to her plot today?</h1>
+            <h1>🌻 Can Lizzi go to her plot today? 🌾</h1>
             <TodayCheck />
             <Upcoming />
         </main>
