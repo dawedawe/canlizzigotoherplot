73c5e8433e74f620d83e23367f445a5018349ed2
dawe <dawedawe@posteo.de>
2024-08-02 16:47:55 +0200

    handle scraping errors more gracefully

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -20,43 +20,47 @@ fn download_html(url: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(body)
 }
 
-fn main() {
-    let url = "https://www.rheinenergiestadion.de/termine";
-    let html = download_html(url).unwrap();
+fn scrape_html(fragment: Html) -> Result<String, Box<dyn std::error::Error>> {
+    let event_selector = scraper::Selector::parse("div.events_list_item")?;
 
-    let fragment = Html::parse_fragment(&html);
-    let event_selector = scraper::Selector::parse("div.events_list_item").unwrap();
-
-    let event_details_selector = scraper::Selector::parse("div.events_list_item_text").unwrap();
-    let date_selector = scraper::Selector::parse("b").unwrap();
-    let name_selector = scraper::Selector::parse("h1").unwrap();
+    let event_details_selector = scraper::Selector::parse("div.events_list_item_text")?;
+    let date_selector = scraper::Selector::parse("b")?;
+    let name_selector = scraper::Selector::parse("h1")?;
 
     let mut cal = Cal { cal: Vec::new() };
 
     for event_element in fragment.select(&event_selector) {
         let mut event_details = event_element.select(&event_details_selector);
-        let date_element = event_details.next().unwrap();
-        let name_element = event_details.next().unwrap();
+        let date_element = event_details.next().expect("failed to scrape date");
+        let name_element = event_details.next().expect("failed to scrape name");
 
         let date = date_element
             .select(&date_selector)
             .next()
-            .unwrap()
+            .expect("failed to scrape date")
             .text()
             .collect::<String>();
         let name = name_element
             .select(&name_selector)
             .next()
-            .unwrap()
+            .expect("failed to scrape date")
             .text()
             .collect::<String>();
-        let date = Date::strptime("%d/%m/%Y", date).unwrap();
+        let date = Date::strptime("%d/%m/%Y", date)?;
         let date = format!("{}", date.strftime("%Y-%m-%d"));
         let e = Event { date, name };
         cal.cal.push(e);
     }
 
-    let json = serde_json::to_string(&cal).unwrap();
+    let json = serde_json::to_string(&cal)?;
+    return Ok(json);
+}
+
+fn main() {
+    let url = "https://www.rheinenergiestadion.de/termine";
+    let html = download_html(url).unwrap();
+    let fragment = Html::parse_fragment(&html);
+    let json = scrape_html(fragment).expect("failed to scrape the html");
     let mut file = File::create("cal.json").unwrap();
     file.write_all(json.as_bytes()).unwrap();
 }
