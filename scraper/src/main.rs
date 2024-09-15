02bf3a9e9f09e6a9083faa653ddfa6e314c7e487
dawe <dawedawe@posteo.de>
2024-09-15 23:46:16 +0200

    use ical files as data source

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,9 +1,11 @@
 use ical::parser::Component;
 use jiff::civil::Date;
 use jiff::Timestamp;
+use jiff::Zoned;
 use scraper::Html;
 use std::collections::HashSet;
 use std::fs::File;
+use std::hash::RandomState;
 use std::io::BufReader;
 use std::io::Write;
 use std::str::FromStr;
@@ -12,70 +14,19 @@ use std::str::FromStr;
 struct Event {
     date: String,
     name: String,
+    url: String,
 }
-
-// impl std::hash::Hash for Event {
-//     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-//         self.date.hash(state);
-//         self.name.hash(state);
-//     }
-// }
-
-// impl PartialEq for Event {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.date == other.date && self.name == other.name
-//     }
-// }
-
-// impl Eq for Event {
-
-// }
 
 #[derive(serde::Serialize)]
 struct Cal {
     cal: Vec<Event>,
 }
 
-fn download_html(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+fn download(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!("downloading {}", url);
     let response = reqwest::blocking::get(url)?;
     let body = response.text()?;
     Ok(body)
-}
-
-fn scrape_html(fragment: &Html) -> Result<Vec<Event>, Box<dyn std::error::Error>> {
-    let event_selector = scraper::Selector::parse("div.events_list_item")?;
-
-    let event_details_selector = scraper::Selector::parse("div.events_list_item_text")?;
-    let date_selector = scraper::Selector::parse("b")?;
-    let name_selector = scraper::Selector::parse("h1")?;
-
-    let mut events = Vec::new();
-
-    for event_element in fragment.select(&event_selector) {
-        let mut event_details = event_element.select(&event_details_selector);
-        let date_element = event_details.next().expect("failed to scrape date");
-        let name_element = event_details.next().expect("failed to scrape name");
-
-        let date = date_element
-            .select(&date_selector)
-            .next()
-            .expect("failed to scrape date")
-            .text()
-            .collect::<String>();
-        let name = name_element
-            .select(&name_selector)
-            .next()
-            .expect("failed to scrape date")
-            .text()
-            .collect::<String>();
-        let name = name.replace("–", "-");
-        let date = Date::strptime("%d/%m/%Y", date)?;
-        let date = format!("{}", date.strftime("%Y-%m-%d"));
-        let e = Event { date, name };
-        events.push(e);
-    }
-
-    Ok(events)
 }
 
 fn parse_ical(s: String) -> Vec<Event> {
@@ -88,34 +39,41 @@ fn parse_ical(s: String) -> Vec<Event> {
             _ => panic!(""),
         };
         for event in ical_events {
-            let summary_prop = event
-                .get_property("SUMMARY")
-                .expect("failed to get SUMMARY property");
-            let summary: &String = summary_prop
-                .value
-                .as_ref()
-                .expect("failed to get SUMMARY value");
             let start_prop = event
                 .get_property("DTSTART")
                 .expect("failed to get DTSTART property");
-            let start: &String = start_prop
+            let start_value: &String = start_prop
                 .value
                 .as_ref()
                 .expect("failed to get DTSTART value");
-            let date = match Timestamp::from_str(start) {
+            let date = match Timestamp::from_str(start_value) {
                 Ok(t) => {
                     format!("{}", t.strftime("%Y-%m-%d"))
                 }
                 Err(_) => {
-                    let date = Date::from_str(start)
+                    let date = Date::from_str(start_value)
                         .expect("failed to parse Timestamp from start propery");
                     format!("{}", date.strftime("%Y-%m-%d"))
                 }
             };
 
+            let url_prop = event
+                .get_property("URL")
+                .expect("failed to get URL property");
+            let url_value: &String = url_prop.value.as_ref().expect("failed to get URL value");
+
+            let summary_prop = event
+                .get_property("SUMMARY")
+                .expect("failed to get SUMMARY property");
+            let summary_value: &String = summary_prop
+                .value
+                .as_ref()
+                .expect("failed to get SUMMARY value");
+
             let e = Event {
                 date,
-                name: summary.clone(),
+                name: summary_value.clone(),
+                url: url_value.clone(),
             };
             events.push(e);
         }
@@ -124,9 +82,9 @@ fn parse_ical(s: String) -> Vec<Event> {
     events
 }
 
-fn scrape_ical(fragment: &Html) -> Result<String, Box<dyn std::error::Error>> {
-    let cal_selector = scraper::Selector::parse("div.termine-cal")?;
-    let a_selector = scraper::Selector::parse("a")?;
+fn scrape_ical_links(fragment: &Html) -> Vec<&str> {
+    let cal_selector = scraper::Selector::parse("div.termine-cal").expect("::parse failed");
+    let a_selector = scraper::Selector::parse("a").expect("::parse failed");
     let mut ical_links = HashSet::new();
     for x in fragment.select(&cal_selector) {
         let mut a = x.select(&a_selector);
@@ -138,61 +96,52 @@ fn scrape_ical(fragment: &Html) -> Result<String, Box<dyn std::error::Error>> {
         ical_links.insert(link);
     }
 
-    if ical_links.len() > 1 {
-        let mut i = 0;
-        ical_links.into_iter().for_each(|f| {
-            let fname = format!("ical{}.ical", i);
-            let mut file = File::create(fname).expect("failed to create file");
-            file.write_all(f.as_bytes()).expect("failed to write file");
-            i = i+1;
-        });
-
-        panic!("differing ical files found"); // yes, they currently provide the same ical file for all events
-    }
-    let link = ical_links.iter().next().expect("no ical files found");
-    let ical_file = download_html(link)?;
-    Ok(ical_file)
+    ical_links.into_iter().collect()
 }
 
-fn merge(events1: Vec<Event>, events2: Vec<Event>) -> Cal {
-    let mut events: HashSet<Event> = HashSet::new();
-    // let x1 = Event { name: "foo".to_string(), date: "2024-10-05".to_string()};
-    // let x2 = Event { name: "foo".to_string(), date: "2024-10-05".to_string()};
-    // events.insert(x1);
-    // events.insert(x2);
-    events1.into_iter().for_each(|e| {
-        events.insert(e);
-        ()
+fn get_events_from_ical_link(ical_url: &str) -> Vec<Event> {
+    let ical_content = download(ical_url).unwrap();
+    parse_ical(ical_content)
+}
+
+fn get_events_from_ical_links(ical_urls: Vec<&str>) -> Vec<Event> {
+    let mut events: Vec<Event> = Vec::new();
+
+    ical_urls.into_iter().for_each(|url| {
+        let ical_events = get_events_from_ical_link(url);
+        ical_events.into_iter().for_each(|e| events.push(e));
     });
-    println!("after events1 {}", events.len());
-    events2.into_iter().for_each(|e| {
-        events.insert(e);
-        ()
-    });
-    println!("after events2 {}", events.len());
-    let cal_events: Vec<Event> = events.into_iter().collect();
-    Cal { cal: cal_events }
+
+    events
+}
+
+fn filter_events(events: Vec<Event>) -> Vec<Event> {
+    let unique_events: HashSet<Event, RandomState> = HashSet::from_iter(events);
+    let current_date = Zoned::now().date();
+    let mut future_events: Vec<Event> = unique_events
+        .into_iter()
+        .filter(|event| {
+            let date =
+                Date::from_str(&event.date).expect("failed to parse Timestamp from start propery");
+            date >= current_date
+        })
+        .collect();
+
+    future_events.sort_by_cached_key(|e| Date::from_str(&e.date).unwrap());
+    future_events
 }
 
 fn main() {
     let url = "https://www.rheinenergiestadion.de/termine";
-    let html = download_html(url).expect("failed to download html");
+    let html = download(url).expect("failed to download html");
     let fragment = Html::parse_fragment(&html);
-    let ical = scrape_ical(&fragment).unwrap();
-    let ical_events = parse_ical(ical);
-    println!("ical------");
+    let ical_links = scrape_ical_links(&fragment);
+    let ical_events = get_events_from_ical_links(ical_links);
+    let ical_events = filter_events(ical_events);
     ical_events
         .iter()
-        .for_each(|e| println!("{} {}", e.name, e.date));
-    let website_events = scrape_html(&fragment).expect("failed to scrape html");
-    println!("website------");
-    website_events
-        .iter()
-        .for_each(|e| println!("{} {}", e.name, e.date));
-    println!("found {} events on website", website_events.len());
-    println!("found {} events in ical file", ical_events.len());
-    let cal = merge(website_events, ical_events);
-    println!("{} merged events", cal.cal.len());
+        .for_each(|e| println!("{} {}", e.date, e.name));
+    let cal = Cal { cal: ical_events };
     let json = serde_json::to_string(&cal).expect("failed to serialize");
     let mut file = File::create("cal.json").expect("failed to create file");
     file.write_all(json.as_bytes())
